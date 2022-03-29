3a6467679ec48f7fb1e5ad000a669c98226796b2
wykee2 <wycliffnjenga19@gmail.com>
2022-03-29 16:14:01 +0300

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Wykee Njenga on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
