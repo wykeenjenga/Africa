ba6cde82c09eaba82c4c44a620378de36761dd31
wykee2 <wycliffnjenga19@gmail.com>
2022-03-30 18:05:23 +0300

    Updated

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Wykee Njenga on 3/29/22.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable{
+    let id: Int
+    let name: String
+}
